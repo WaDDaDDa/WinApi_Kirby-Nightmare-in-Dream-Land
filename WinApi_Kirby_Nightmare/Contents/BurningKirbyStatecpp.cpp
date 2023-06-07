e5579556cc22ddd93df55d695e09bec4aa9eca05
KimJunYoung <kimchk1104@naver.com>
2023-06-07 18:37:40 +0900

    김준영 : 업데이트

--- WinApi_Kirby_Nightmare/Contents/BurningKirbyStatecpp.cpp
+++ WinApi_Kirby_Nightmare/Contents/BurningKirbyStatecpp.cpp
@@ -3,10 +3,67 @@
 
 void BurningKirby::AttackStartUpdate(float _Delta)
 {
+	if (GetLiveTime() >= 0.6f)
+	{
+		ChangeState(KirbyState::Idle);
+		return;
+	}
 
+	float4 TackleSpeed = { 2.0f , 0.0f };
+	if (KirbyDir::Left == Dir)
+	{
+		CheckPos = { -40.0f, -40.0f };
+		if (GetWallCheck() != RGB(255, 255, 255))
+		{
+			MovePos.X *= 0;
+			AddPos(MovePos);
+			return;
+		}
+		AddPos(-TackleSpeed);
+	}
+	else if (KirbyDir::Right == Dir)
+	{
+		CheckPos = { 40.0f, -40.0f };
+		if (GetWallCheck() != RGB(255, 255, 255))
+		{
+			MovePos.X *= 0;
+			AddPos(MovePos);
+			return;
+		}
+		AddPos(TackleSpeed);
+	}
 }
 
 void BurningKirby::AttackUpdate(float _Delta)
 {
 
+	if (GetLiveTime() >= 0.6f)
+	{
+		ChangeState(KirbyState::Idle);
+		return;
+	}
+
+	float4 TackleSpeed = { 1.0f , 0.0f };
+	if (KirbyDir::Left == Dir)
+	{
+		CheckPos = { -40.0f, -40.0f };
+		if (GetWallCheck() != RGB(255, 255, 255))
+		{
+			MovePos.X *= 0;
+			AddPos(MovePos);
+			return;
+		}
+		AddPos(-TackleSpeed);
+	}
+	else if (KirbyDir::Right == Dir)
+	{
+		CheckPos = { 40.0f, -40.0f };
+		if (GetWallCheck() != RGB(255, 255, 255))
+		{
+			MovePos.X *= 0;
+			AddPos(MovePos);
+			return;
+		}
+		AddPos(TackleSpeed);
+	}
 }
